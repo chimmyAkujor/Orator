3fa4f043d64561acc4872c1b444349b0512b1d99
Chimere Akujor <sincerelypiper@gmail.com>
2019-09-24 01:08:52 -0500

    minor tweaks

--- twitter_bot/webhook_scripts/create-webhook.py
+++ twitter_bot/webhook_scripts/create-webhook.py
@@ -6,13 +6,15 @@ CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
 ACCESS_KEY = os.environ.get('TWITTER_ACCESS_TOKEN')
 ACCESS_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+PAYLOAD_URL = os.environ.get('TWITTER_PAYLOAD_URL')
+
 
 
 url = "https://api.twitter.com/1.1/account_activity/all/prod/webhooks.json"
 
 auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_KEY, ACCESS_SECRET)
 
-payload = {'url':'https://62ce9fb5.ngrok.io/webhook/twitter'}
+payload = {'url':PAYLOAD_URL}
 
 response = requests.post(url,auth=auth,params = payload)
 
