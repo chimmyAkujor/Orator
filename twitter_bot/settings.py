0dd2659436afc87579317379934f2280aac1d483
Chimere Akujor <sincerelypiper@gmail.com>
2023-07-09 14:52:08 -0500

    Update settings.py

--- twitter_bot/settings.py
+++ twitter_bot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2799@yz8ly4zplcdq-^oqg+=4ofnbmnelp@yq(k5cpbig$6-yn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
